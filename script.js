21a55747f5a43f60a5c1bef986a409f3ce4839fe
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-09 09:48:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function PT(path, callback) {
-  fetch('https://'+localStorage.getItem('domain')+'/api/client/'+path, {
-    headers: {
-      authorization: 'Bearer '+localStorage.getItem('key')
-    }
-  }).then(async e => {
+  fetch('https://api.fsh.plus/pt?url=https://'+localStorage.getItem('domain')+'/api/client/'+path+'&key='+localStorage.getItem('key')).then(async e => {
     e = await e.json();
     callback(e)
   })
