3d94facfad093e6748d925f6aa9537c1f0fe822e
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-09 22:35:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function PT(path, callback) {
-  fetch('https://api.fsh.plus/pt?url=https://'+localStorage.getItem('domain')+'/api/client/'+path+'&key='+localStorage.getItem('key')).then(async e => {
+function PT(path, callback, method = 'GET', body = '') {
+  fetch('https://api.fsh.plus/pt?url=https://'+localStorage.getItem('domain')+'/api/client/'+path+'&key='+localStorage.getItem('key')+'&method='+method+'&body='+body).then(async e => {
     e = await e.json();
     callback(e)
   })
