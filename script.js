7375de9d7958d75b38b00101e8489097b51079c5
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-09 21:13:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function PT(path, callback) {
     callback(e)
   })
 }
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
