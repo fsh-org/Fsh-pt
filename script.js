e66df84dd6fdb545882f5b2062fbe7551b16d8a5
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-09 09:37:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function PT(path, callback) {
-    fetch('https://'+localStorage.getItem('domain')+'/api/client/'+path, {
-        headers: {
-            authorization: 'Bearer '+localStorage.getItem('key')
-        }
-    }).then(async e => {
-        callback()
-    })
+  fetch('https://'+localStorage.getItem('domain')+'/api/client/'+path, {
+    headers: {
+      authorization: 'Bearer '+localStorage.getItem('key')
+    }
+  }).then(async e => {
+    e = await e.json();
+    callback(e)
+  })
 }
