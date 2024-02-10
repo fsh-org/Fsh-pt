939b923c748e986c86eb06c48b0b18deec557e51
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-10 22:11:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function formatBytes(bytes, decimals = 2) {
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + sizes[i];
 }
+function __PRI__edr(er,tr) {
+  if (er == 0) {
+    return '';
+  } else {
+    return er+' '+tr+(er>1?'s':'')+' '
+  }
+}
+function time_gud(time) {
+  return `${__PRI__edr(Math.floor(time / 31536000000),'millennium')}${__PRI__edr(Math.floor(time / 31536000 % 1000),'year')}${__PRI__edr(Math.floor(time % 31536000 / 604800),'week')}${__PRI__edr(Math.floor(time / 86400) % 7,'day')}${__PRI__edr(Math.floor(time / 3600) % 24,'hour')}${__PRI__edr(Math.floor(time / 60) % 60,'minute')}${__PRI__edr(time % 60,'second')}`
+}
