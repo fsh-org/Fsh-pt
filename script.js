ed2a6ad1964510e15f79c2fe93bdfd2b6b4ab3eb
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-11 17:30:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Functions
 function PT(path, callback, method = 'GET', body = '') {
   fetch('https://api.fsh.plus/pt?url=https://'+localStorage.getItem('domain')+'/api/client/'+path+'&key='+localStorage.getItem('key')+'&method='+method+'&body='+body).then(async e => {
     e = await e.json();
@@ -21,4 +22,11 @@ function __PRI__edr(er,tr) {
 }
 function time_gud(time) {
   return `${__PRI__edr(Math.floor(time / 31536000),'Y')}${__PRI__edr(Math.floor(time % 31536000 / 604800),'W')}${__PRI__edr(Math.floor(time / 86400) % 7,'d')}${__PRI__edr(Math.floor(time / 3600) % 24,'h')}${__PRI__edr(Math.floor(time / 60) % 60,'m')}${__PRI__edr(time % 60,'s')}`
+}
+
+// Checks
+if (location.pathname != '/') {
+  if (!localStorage.getItem('key')) {
+    location.pathname == '/';
+  }
 }
