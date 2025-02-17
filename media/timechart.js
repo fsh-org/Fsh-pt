43ccf865fc7c84940e1e9ab73d121d190f31bbae
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-02-17 12:26:31 +0100

    More chart work

--- media/timechart.js
+++ media/timechart.js
@@ -2,22 +2,30 @@ class Chart {
   constructor(id) {
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext('2d');
-    this.points = 19; // 0 Indexed
+    this.points = 20;
+    this.lineColor = getComputedStyle(document.documentElement).getPropertyValue("--blue-1").trim();
   }
   draw(data) {
     // Set canvas size
-    this.canvas.setAttribute('width', 300);
-    this.canvas.setAttribute('height', 150);
+    const width = 300;
+    const height = 200;
+    this.canvas.setAttribute('width', width);
+    this.canvas.setAttribute('height', height);
+
+    // Scale values
+    let dataMin = Math.min(...data);
+    let dataDif = Math.max(...data) - dataMin;
+    data = data.map(num => height - ((num - dataMin) / dataDif) * height * 0.8);
 
     // Clear
-    this.ctx.clearRect(0, 0, 300, 150)
+    this.ctx.clearRect(0, 0, width, height);
 
     // Draw lines
     this.ctx.beginPath();
-    this.ctx.strokeStyle = getComputedStyle(document.documentElement).getPropertyValue("--blue-1").trim();
-    this.ctx.moveTo(data[points].x, data[points].y);
-    for (let i = points-1; i > 0; i--) {
-      this.ctx.lineTo(data[i].x, data[i].y);
+    this.ctx.strokeStyle = this.lineColor;
+    this.ctx.moveTo(width, data[0]);
+    for (let i = 1; i > data.length-1; i++) {
+      this.ctx.lineTo((width/this.points)*i, (data[i]??height));
     }
     this.ctx.lineWidth = 2;
     this.ctx.stroke();
